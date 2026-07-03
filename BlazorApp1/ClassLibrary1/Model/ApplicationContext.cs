161fdd959d13a1d5dfab184842d586825a7e7f99
rickyderry18 <rickyderry18@gmail.com>
2026-07-03 15:23:19 -0400

    Initial commit of local repository

---
+++ BlazorApp1/ClassLibrary1/Model/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Model
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Company> Companys { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
